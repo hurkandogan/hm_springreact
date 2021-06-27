68fafa978d9172fe9851840f4303c26afadd99be
Hurkan Dogan <60603139+hurkandogan@users.noreply.github.com>
2021-06-27 02:31:47 +0200

    Initial commit

--- src/main/java/com/hugos/hm/controller/ArtworkController.java
+++ src/main/java/com/hugos/hm/controller/ArtworkController.java
@@ -4,11 +4,14 @@ import com.hugos.hm.model.Artwork;
 import com.hugos.hm.repository.ArtworkRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class ArtworkController {
 
     private final ArtworkRepo artworkRepo;
@@ -19,9 +22,14 @@ public class ArtworkController {
     }
 
     @GetMapping("/artwork")
-    List<Artwork> all(){
-        return artworkRepo.findAll();
+    Collection<Artwork> all(){
+        Collection<Artwork> test = artworkRepo.findAll();
+        System.out.println(test);
+        return test;
     }
+
+    //@GetMapping("/artwork/{id}")
+
 
     //TODO: POST, UPDATE, PUT, DELETE
 }
